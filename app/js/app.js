3e42af58d78aba911b03be515e607a8d67aa77dd
Natali2707 <105042840+Natali2707@users.noreply.github.com>
2022-06-30 03:17:39 +0300

    add mixin, breakpoints

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,14 @@
-// // Import vendor jQuery plugin example
-// import '~/app/libs/mmenu/dist/mmenu.js'
 
-document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+console.log("hello");
+console.log("hello world");
+let header = document.querySelector(".header");
 
-})
+document.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 1) {
+    header.classList.add("header_has-bg");
+  } else {
+    header.classList.remove("header_has-bg");
+  }
+});
+
